0b3e00bb0058edc5800cedef6229237af0aa7d14
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-03-21 11:55:14 -0700

    added array object 5

---
+++ arrOfObj.js
@@ -0,0 +1,8 @@
+let people = [
+    {'name': 'jon', 'jobTitle': 'Manager'},
+    {'name': 'sasha', 'jobTitle': 'IT Manager'},
+    {'name': 'bill', 'jobTitle': 'Business Manager'}
+]
+for(let key in people){
+    console.log(people[key]);
+}
