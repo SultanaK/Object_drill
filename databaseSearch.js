9de65baf88ed3827e774d2acfc724c33d7d4ba36
Your name here <kr.shampa@yahoo.com>
2020-03-23 23:38:25 -0500

    adding findOne

---
+++ databaseSearch.js
@@ -0,0 +1,21 @@
+const HEROES = [
+    { id: 1, name: 'Captain America', squad: 'Avengers' },
+    { id: 2, name: 'Iron Man', squad: 'Avengers' },
+    { id: 3, name: 'Spiderman', squad: 'Avengers' },
+    { id: 4, name: 'Superman', squad: 'Justice League' },
+    { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+    { id: 6, name: 'Aquaman', squad: 'Justice League' },
+    { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+//console.log(Object.values(HEROES));
+function findOne(arr, query) {
+  for(let i=0; i<arr.length; i++){
+      //console.log(arr[i].id)
+      //console.log(Object.values(query))
+      if(arr[i].id === Object.values(query)){
+          return arr[i];
+       } else return null; 
+  } 
+  
+}
+findOne(HEROES, { id: 1 });
